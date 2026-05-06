6c6335c66cb793cf22a8bf80d47ef79728d64ee5
Ubuntu <ubuntu@ip-172-31-41-80.eu-north-1.compute.internal>
2026-05-06 20:02:42 +0000

    Aktualizacja: działające połączenie z RDS

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# TWOJE HASLO ZAWSZE Z %21 ZAMIAST WYKRZYKNIKA
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:Admin123%21@chmura-baza.cliwkweeidej.eu-north-1.rds.amazonaws.com:3306/rezerwacje'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Rezerwacja(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    imie = db.Column(db.String(100), nullable=False)
+    usluga = db.Column(db.String(100), nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def index():
@@ -10,9 +23,10 @@ def index():
 def rezerwuj():
     imie = request.form.get('imie')
     usluga = request.form.get('usluga')
-    
-    # Pozniej bedzie dodany kod zapisujacy dane do bazy AWS RDS
-    return f"{imie}, rezerwacja na usluge: {usluga} została wstepnie przyjeta!"
+    nowa_rezerwacja = Rezerwacja(imie=imie, usluga=usluga)
+    db.session.add(nowa_rezerwacja)
+    db.session.commit()
+    return f"{imie}, rezerwacja na usluge: {usluga} została pomyślnie zapisana w chmurze AWS RDS!"
 
 if __name__ == '__main__':
     app.run(debug=True)
